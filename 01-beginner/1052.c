5b1f4f7fa2433fb27b974c1641cbc39c4066b28d
marcos.lima@icomp.ufam.edu.br <marcos.lima@icomp.ufam.edu.br>
2019-12-23 17:31:21 -0400

    Add solution for problem 1052 Month

---
+++ 01-beginner/1052.c
@@ -0,0 +1,72 @@
+/*
+URI Online Judge | 1052
+Month
+Adapted by Neilor Tonin, URI  Brazil
+https://www.urionlinejudge.com.br/judge/en/problems/view/1052
+
+Timelimit: 1
+
+Read an integer number between 1 and 12, including. Corresponding to this number, you must print the month of the year, in english, with the first letter in uppercase.
+
+Input
+The input contains only an integer number.
+
+Output
+Print the name of the month according to the input number, with the first letter in uppercase.
+
+@author Marcos Lima
+@profile https://www.urionlinejudge.com.br/judge/pt/profile/242402
+@status Accepted
+@language C (gcc 4.8.5, -O2 -lm) [+0s]
+@time 0.000s
+@size 981 Bytes
+@submission 12/7/19, 1:32:31 PM
+*/
+#include <stdio.h>
+
+int main()
+{
+    unsigned char n;
+    scanf("%hhu", &n);
+    
+    switch (n) {
+        case 1:
+            printf("January\n");
+            break;
+        case 2:
+            printf("February\n");
+            break;
+        case 3:
+            printf("March\n");
+            break;
+        case 4:
+            printf("April\n");
+            break;
+        case 5:
+            printf("May\n");
+            break;
+        case 6:
+            printf("June\n");
+            break;
+        case 7:
+            printf("July\n");
+            break;
+        case 8:
+            printf("August\n");
+            break;
+        case 9:
+            printf("September\n");
+            break;
+        case 10:
+            printf("October\n");
+            break;
+        case 11:
+            printf("November\n");
+            break;
+        case 12:
+            printf("December\n");
+            break;
+    }
+    
+    return 0;
+}
